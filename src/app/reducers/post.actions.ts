02d6fb5d5eaf95fd5b8989db2b7647cb8e2594ef
Gai <german.isaev@gmail.com>
2021-01-20 13:10:51 +0200

    Update post.actions.ts

--- src/app/reducers/post.actions.ts
+++ src/app/reducers/post.actions.ts
@@ -1,8 +1,8 @@
 import { Action } from '@ngrx/store';
 
 export const EDIT_TEXT = '[Post] Edit';
-export const UPVOTE = '[Post] Upvote';
-export const DOWNVOTE = '[Post] Downvote';
+export const UPVOTE = '[Post] UpVote';
+export const DOWNVOTE = '[Post] DownVote';
 export const RESET = '[Post] Reset';
 
 export class EditText implements Action {
