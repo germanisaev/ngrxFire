d9e8b1787d69e73f1ababe61c882cc0fbfb80be8
Gai <german.isaev@gmail.com>
2021-01-20 13:01:40 +0200

    Update admin-routing.module.ts

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -5,10 +5,6 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'login', pathMatch: 'full'
-    },
-    {
         path: "login",
         component: LoginComponent
     },
