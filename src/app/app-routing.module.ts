85e76b9b61f0f88b437b24380f28bc2f19d51f53
Gai <german.isaev@gmail.com>
2021-01-20 13:02:38 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-//import { LoginComponent } from './admin/login/login.component';
 import { ContactComponent } from './contact/contact.component';
 
 
